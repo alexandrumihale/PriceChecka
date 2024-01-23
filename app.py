ff2427f190943ad54b752d7b208367d09a96d197
Alexandru Mihale <alexandru.mihale@netcentric.biz>
2024-01-23 23:22:00 +0200

    Gave better structure for future expansion

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
-import telegram_send
-import asyncio
-
-from date import * 
+from imports import *
+from date import *
 
 
 def get_emag_price(product_url, output_file='emag_price.txt'):
@@ -14,16 +10,19 @@ def get_emag_price(product_url, output_file='emag_price.txt'):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Price Element
+        # Product elements
         price_element = soup.find('p', class_='product-new-price')
+        name_element = soup.find('h1', class_='page-title')
+
 
         # Check if the element was found
-        if price_element:
+        if price_element and name_element:
             price = price_element.text.strip()
+            name = name_element.text.strip()
 
            # Save the price to the specified output file
             with open(output_file, 'a+') as file:
-                file.write(f'{logdate} {price} \n')
+                file.write(f'{logdate} {name} {price} \n')
 
             print(f'The price of the product is: {price}. Price saved to {output_file}')
             asyncio.run(telegram_send.send(messages=[f'The price of the product is: {price}. Price saved to {output_file}']))
@@ -33,5 +32,7 @@ def get_emag_price(product_url, output_file='emag_price.txt'):
         print(f'Failed to fetch the page. Status code: {response.status_code}')
 
 # Example usage
-product_url = input("Please enter emag product URL:")
-get_emag_price(product_url)
+#product_url = input("Please enter emag product URL:")
+example = "https://www.emag.ro/cos-de-gunoi-15-l-incorporabil-cu-capac-automat-gri-antracit-5618/pd/DC3L9QBBM/?ref=hdr-favorite_products"
+
+get_emag_price(example)
--- date.py
+++ date.py
@@ -7,6 +7,6 @@ day = today.day
 month = today.month
 year = today.year
 
-current_time = rightnow.strftime('%H:%M')
+current_time = rightnow.strftime('%H:%M:')
 current_day = f'{day}/{month}/{year}'
 logdate = f'{current_day} {current_time}'
